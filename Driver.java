6ed15b173e5ca5247d2ef42f2c6d9616424bbd23
Prattay <pdey30@stuy.edu>
2022-01-06 12:31:51 -0500

    init setup

--- Driver.java
+++ Driver.java
@@ -1,1 +1,51 @@
+// Peanut Butter and Nutella - Prattay Dey + Winnie, Brian Li + Robert, Nafiz Labib + Martha
+// APCS pd6
+// Lab04 -- Sort Algorithms
+// 2022-01-06
+// time spent: 0.6 hrs
 
+import java.util.ArrayList;
+
+public class Driver
+{
+  //~~~~~~~~~~~~~~~~~~~ HELPER METHODS ~~~~~~~~~~~~~~~~~~~
+  //precond: lo < hi && size > 0
+  //postcond: returns an ArrayList of random integers
+  //          from lo to hi, inclusive
+  public static ArrayList populate( int size, int lo, int hi ) {
+    ArrayList<Integer> retAL = new ArrayList<Integer>();
+    while( size > 0 ) {
+      //     offset + rand int on interval [lo,hi]
+      retAL.add( lo + (int)( (hi-lo+1) * Math.random() ) );
+      size--;
+    }
+    return retAL;
+  }
+
+  //randomly rearrange elements of an ArrayList
+  public static void shuffle( ArrayList al ) {
+    int randomIndex;
+    for( int i = al.size()-1; i > 0; i-- ) {
+      //pick an index at random
+      randomIndex = (int)( (i+1) * Math.random() );
+      //swap the values at position i and randomIndex
+      al.set( i, al.set( randomIndex, al.get(i) ) );
+    }
+  }
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  public static void main( String[] args ){
+    // Bubble Sort
+      // General case
+    ArrayList nafiz = new ArrayList<Integer>();
+    nafiz.add(7);
+    nafiz.add(1);
+    nafiz.add(5);
+    nafiz.add(12);
+    nafiz.add(3);
+    Sorts.bubble(nafiz);
+
+
+  }
+
+}
--- Sorts.java
+++ Sorts.java
@@ -1,1 +1,112 @@
+// Peanut Butter and Nutella - Prattay Dey + Winnie, Brian Li + Robert, Nafiz Labib + Martha
+// APCS pd6
+// Lab04 -- Sort Algorithms
+// 2022-01-06
+// time spent: 0.6 hrs
 
+import java.util.ArrayList;
+
+public class Sorts
+{
+  // VOID version of bubbleSort
+  // Rearranges elements of input ArrayList
+  // postcondition: data's elements sorted in ascending order
+  public static void bubble( ArrayList<Comparable> data )
+  {
+    // outside for loop allowing for maximum traversal in ascending order
+    // (a value at index 0 can move all the way to the largest index in the dataset)
+    for (int i = 0; i < data.size(); i++){
+
+      // from the last index, sorts until reaching index i in descending order
+      // (because after pass 0, the value at index 0 will be correct.
+      // it becomes pointless to search for a potential sort at that index again.)
+      for (int n = data.size() - 1; n > i; n--){
+
+        // if the value at index n is less than the value at index n-1, swap
+        if ((data.get(n)).compareTo(data.get(n-1)) < 0){
+          Comparable temp = data.get(n);
+          data.set(n, data.get(n-1));
+          data.set(n-1, temp);
+        }
+      }
+    }
+  }
+
+
+  // VOID version of SelectionSort
+  // Rearranges elements of input ArrayList
+  // postcondition: data's elements sorted in ascending order
+  public static void selection( ArrayList<Comparable> data )
+  {
+    //note: this version places greatest value at "rightmost" end
+
+    //maxPos will point to position of SELECTION (greatest value)
+    int maxPos;
+
+    // iterates until pass is 0
+    for(int pass = data.size() - 1; pass > 0; pass--) {
+      // System.out.println( "\nbegin pass " + (data.size()-pass) );//diag
+      maxPos = 0;
+
+      // iterates until i <= pass, because the value at index pass+1 will already
+      // have the correct value in it, as a result of the previous iteration of the
+      // above for loop.
+      for(int i = 0; i <= pass; i++) {
+
+        // if the value at index maxPos < value at index i, update maxPos
+        if ((data.get(maxPos)).compareTo(data.get(i)) < 0){
+          maxPos = i;
+        }
+
+        // System.out.println( "maxPos: " + maxPos );//diag
+        // System.out.println( data );//diag
+
+      }
+
+      // swap
+      Comparable temp = data.get(pass);
+      data.set(pass, data.get(maxPos));
+      data.set(maxPos, temp);
+
+      // System.out.println( "after swap: " +  data );//diag
+    }
+  }//end selection
+
+
+
+  // VOID version of InsertionSort
+  // Rearranges elements of input ArrayList
+  // postcondition: data's elements sorted in ascending order
+  public static void insertion( ArrayList<Comparable> data )
+  {
+    for(int partition = 0; partition < data.size(); partition++) {
+      //partition marks first item in unsorted region
+
+      // System.out.println( "\npartition: " + partition + "\tdataset:"); //diag
+      // System.out.println( data );
+
+      //traverse sorted region from right to left
+      for(int i = partition; i > 0; i--) {
+
+        // "walk" the current item to where it belongs
+        // by swapping adjacent items
+
+        // if index i < index i-1, swap
+        if ((data.get(i)).compareTo(data.get(i-1)) < 0) {
+
+          // System.out.println( "swap indices "+(i-1)+" & "+i+"..." ); //diag
+
+          // swap
+          Comparable temp = data.get(i);
+          data.set(i, data.get(i-1));
+          data.set(i-1, temp);
+
+        }
+        else
+          break;
+      }
+    }
+  }//end insertion
+
+
+}
